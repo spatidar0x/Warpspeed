d0606a2aede636759d3c17c01d7a4837e67ef5fb
spatidar0x <0xsunil.patidar@gmail.com>
2022-05-29 11:13:37 +0530

    fourth commit

--- index.js
+++ index.js
@@ -8,58 +8,58 @@ var web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v
 app.get('/getDecodedTransaction', (req, res) => {
     txHash = req.query.txHash;
     web3.eth.getTransaction(txHash, (error, txResult) => {
-        //console.log(txResult);
         if (txResult === '0x') {
-                res.send({
-                message: "Error",
+            res.send({
+                message: "Transaction details not found",
                 methodResponse: "",
                 status: 'Failed'
             });
-        } else if (typeof(txResult) !== 'undefined'){
-            message = txResult.from + ' has transferred ' + txResult.value + ' amount to ' + txResult.to;
-        
-            //res.send(txResult);
+        } else if (typeof (txResult) !== 'undefined') {
+
             const smartContractAddress = txResult.to;
-            url = 'https://api.etherscan.io/api?module=contract&action=getabi&address='+smartContractAddress+'&apikey=A6VYVVB4X374QB72GXJYTK82FQ65NZ2K6Y';
-           // console.log(url);
-    
-           // const UNISWAPAddress = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984';
-            // url = 'https://api.etherscan.io/api?module=contract&action=getabi&address='+UNISWAPAddress+'&apikey=A6VYVVB4X374QB72GXJYTK82FQ65NZ2K6Y';
-    
+          
+            const API_KEY = 'A6VYVVB4X374QB72GXJYTK82FQ65NZ2K6Y';
+            url = 'https://api.etherscan.io/api?module=contract&action=getabi&address=' + smartContractAddress + '&apikey=' + API_KEY;
             axios({
                 method: 'get',
                 url: url
             }).then(function (response) {
-                console.log(response);
                 if (response.data.result === 'Contract source code not verified') {
                     res.send({
-                        message: "Contract source code not verified",
-                        methodResponse: "",
+                        message: message,
+                        methodResponse: "Contract source code not verified",
                         status: 'Failed'
                     });
-                } else {
-                contractABI = JSON.parse(response.data.result);
-                const decoder = new InputDataDecoder(contractABI);
-                const data = txResult.input.trim()
-                .replace(/(?:[\s\S]*MethodID: (.*)[\s\S])?[\s\S]?\[\d\]:(.*)/gi, '$1$2');          
-                const result = decoder.decodeData(data);
-                const methodResponse = {
-                    method: result.method,
-                    argsTypes: result.types,
-                    argNames: result.names
-                };
-                res.send({
-                    message: message,
-                    methodResponse: methodResponse,
-                    status: 'Ok'
-                });
-            }
-                });
+                }
+                else {
+                    contractABI = JSON.parse(response.data.result);
+                    const decoder = new InputDataDecoder(contractABI);
+                    const data = txResult.input.trim()
+                        .replace(/(?:[\s\S]*MethodID: (.*)[\s\S])?[\s\S]?\[\d\]:(.*)/gi, '$1$2');
+                    const result = decoder.decodeData(data);
+                    const methodResponse = {
+                        method: result.method,
+                        argsTypes: result.types,
+                        argNames: result.names
+                    };
+                    let val = web3.utils.fromWei(txResult.value, 'ether');
+                    if (result.method === 'mint') {
+                        message = 'Tokens worth ' + val.toString() + ' ethers have been minted to ' + txResult.to //+ ' with a gas fee of '+  gas.toString() + ' ethers';
+                    } else {
+                        message = txResult.from + ' has transferred ' + val.toString() + ' ethers to ' + txResult.to //+ ' with a gas fee of '+  gas.toString() + ' ethers';
+                    }
+                    res.send({
+                        message: message,
+                        methodResponse: methodResponse,
+                        status: 'Ok'
+                    });
+                }
+            });
         }
-        
+
     });
 });
- 
+
 //PORT ENVIRONMENT VARIABLE
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
