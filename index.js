a4679109cab4480a326fae6d1870fc799d73156a
spatidar0x <0xsunil.patidar@gmail.com>
2022-05-29 09:48:27 +0530

    third commit

--- index.js
+++ index.js
@@ -9,36 +9,45 @@ app.get('/getDecodedTransaction', (req, res) => {
     txHash = req.query.txHash;
     web3.eth.getTransaction(txHash, (error, txResult) => {
         //console.log(txResult);
-        message = txResult.from + ' has transferred ' + txResult.value + ' amount to ' + txResult.to;
+        if (txResult === '0x') {
+                res.send({
+                message: "Error",
+                methodResponse: "",
+                status: 'Failed'
+            });
+        } else {
+            message = txResult.from + ' has transferred ' + txResult.value + ' amount to ' + txResult.to;
         
-        //res.send(txResult);
-        const smartContractAddress = txResult.to;
-        url = 'https://api.etherscan.io/api?module=contract&action=getabi&address='+smartContractAddress+'&apikey=A6VYVVB4X374QB72GXJYTK82FQ65NZ2K6Y';
-        console.log(url);
-
-       // const UNISWAPAddress = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984';
-        // url = 'https://api.etherscan.io/api?module=contract&action=getabi&address='+UNISWAPAddress+'&apikey=A6VYVVB4X374QB72GXJYTK82FQ65NZ2K6Y';
-
-        axios({
-            method: 'get',
-            url: url
-        }).then(function (response) {
-            contractABI = JSON.parse(response.data.result);
-            const decoder = new InputDataDecoder(contractABI);
-            const data = txResult.input.trim()
-            .replace(/(?:[\s\S]*MethodID: (.*)[\s\S])?[\s\S]?\[\d\]:(.*)/gi, '$1$2');          
-            const result = decoder.decodeData(data);
-            const methodResponse = {
-                method: result.method,
-                argsTypes: result.types,
-                argNames: result.names
-            };
-            res.send({
-                message: message,
-                methodResponse: methodResponse,
-                status: 'Ok'
-            });
-            });
+            //res.send(txResult);
+            const smartContractAddress = txResult.to;
+            url = 'https://api.etherscan.io/api?module=contract&action=getabi&address='+smartContractAddress+'&apikey=A6VYVVB4X374QB72GXJYTK82FQ65NZ2K6Y';
+            console.log(url);
+    
+           // const UNISWAPAddress = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984';
+            // url = 'https://api.etherscan.io/api?module=contract&action=getabi&address='+UNISWAPAddress+'&apikey=A6VYVVB4X374QB72GXJYTK82FQ65NZ2K6Y';
+    
+            axios({
+                method: 'get',
+                url: url
+            }).then(function (response) {
+                contractABI = JSON.parse(response.data.result);
+                const decoder = new InputDataDecoder(contractABI);
+                const data = txResult.input.trim()
+                .replace(/(?:[\s\S]*MethodID: (.*)[\s\S])?[\s\S]?\[\d\]:(.*)/gi, '$1$2');          
+                const result = decoder.decodeData(data);
+                const methodResponse = {
+                    method: result.method,
+                    argsTypes: result.types,
+                    argNames: result.names
+                };
+                res.send({
+                    message: message,
+                    methodResponse: methodResponse,
+                    status: 'Ok'
+                });
+                });
+        }
+        
     });
 });
  
